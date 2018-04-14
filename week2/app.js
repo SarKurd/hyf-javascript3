7083b998083e97a877044e7ca4776015cbc7a7d8
SarKurd <sarbast2009@hotmail.co.uk>
2018-04-15 00:53:02 +0200

    applied Jim's feedback

--- week2/app.js
+++ week2/app.js
@@ -2,31 +2,32 @@
 
 function fetchJSON(url) {
     return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.responseType = "json";
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.responseType = "json";
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status < 400) {
-                resolve(xhr.response);
-            } else {
-                reject(new Error(xhr.statusText));
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status < 400) {
+                    resolve(xhr.response);
+                } else {
+                    reject(new Error(`${xhr.status} - Page ${xhr.statusText}`));
 
+                }
             }
         }
-    }
-    xhr.send();
-});
+        xhr.send();
+    });
 }
 
 function renderSelect(repos) {
     const select = document.getElementById("select");
 
     repos.forEach((repo, index) => {
-        const option = createAndAppend("option", select);
-        option.setAttribute("value", index);
-        option.innerHTML = repo.name;
+        createAndAppend("option", select, {
+            html: repo.name,
+            value: index
+        });
     });
 
     renderLeftSide(repos[0]);
@@ -40,16 +41,33 @@ function renderSelect(repos) {
 
 
 function main() {
-    const url = "https://api.github.com/orgs/hackyourfuture/repos";
+    const url = "https://api.github.com/orgs/hackyourfuture/repos?page_size=100";
     const root = document.getElementById("root");
-    const header = createAndAppend("div", root, { class: "header" });
-    createAndAppend("label", header, { class: "select_label", html: "Repositories: " });
-    createAndAppend("select", header, { id: "select" });
-    const container = createAndAppend("div", root, { id: "container" });
-    const leftSideContainer = createAndAppend("div", container, { class: "leftSideContainer" });
-    const rightSideContainer = createAndAppend("div", container, { class: "rightSideContainer" });
-    createAndAppend("h4", rightSideContainer, { html: "Contributors" });
-    createAndAppend("ul", rightSideContainer, { id: "listOfContr" });
+    const header = createAndAppend("div", root, {
+        class: "header"
+    });
+    createAndAppend("label", header, {
+        class: "select_label",
+        html: "Repositories: "
+    });
+    createAndAppend("select", header, {
+        id: "select"
+    });
+    const container = createAndAppend("div", root, {
+        id: "container"
+    });
+    const leftSideContainer = createAndAppend("div", container, {
+        class: "leftSideContainer"
+    });
+    const rightSideContainer = createAndAppend("div", container, {
+        class: "rightSideContainer"
+    });
+    createAndAppend("h4", rightSideContainer, {
+        html: "Contributors"
+    });
+    createAndAppend("ul", rightSideContainer, {
+        id: "listOfContr"
+    });
     const table = createAndAppend("table", leftSideContainer);
     const tBody = createAndAppend("tBody", table);
 
@@ -60,11 +78,15 @@ function main() {
     }
 
     fetchJSON(url)
-    .then(data => {
+        .then(data => {
             renderSelect(data);
         })
-        .catch(err => { 
-            console.error(err.message);
+        .catch(err => {
+            root.style.display = "none";
+            createAndAppend("h1", document.body, {
+                html: err.message,
+                class: "error-message"
+            });
         });
 }
 
@@ -84,28 +106,34 @@ function renderLeftSide(repo) {
 
 function renderRepositories(repo) {
     fetchJSON(repo.contributors_url).then(userData => {
-       
+
             const ul = document.getElementById("listOfContr");
             ul.innerHTML = "";
             for (let i = 0; i < userData.length; i++) {
                 const li = createAndAppend("li", ul);
-                //li.textContent = userData[i].login;
-                const img = createAndAppend("img", li);
-                img.setAttribute("src", userData[i].avatar_url);
-                const span1 = createAndAppend("span", li);
-                span1.setAttribute("class", "span-login");
-                span1.innerHTML = userData[i].login;
+                const img = createAndAppend("img", li, {
+                    "src": userData[i].avatar_url
+                });
+                const span1 = createAndAppend("span", li, {
+                    html: userData[i].login,
+                    "class": "span-login"
+                });
 
-                const span2 = createAndAppend("span", li);
-                span2.setAttribute("class", "span-contributions");
-                span2.innerHTML = userData[i].contributions;
-
-
+                const span2 = createAndAppend("span", li, {
+                    html: userData[i].contributions,
+                    "class": "span-contributions"
+                });
             }
 
         })
         .catch(err => {
-            console.error(err.message);
+            const root = document.getElementById("root");
+            root.style.display = "none";
+            createAndAppend("h1", document.body, {
+                html: err.message,
+                class: "error-message"
+            });
+
         });
 }
 
@@ -113,14 +141,14 @@ function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
     Object.keys(options).forEach(key => {
-      const value = options[key];
-      if (key === 'html') {
-        elem.innerHTML = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
+        const value = options[key];
+        if (key === 'html') {
+            elem.innerHTML = value;
+        } else {
+            elem.setAttribute(key, value);
+        }
     });
     return elem;
-  }
+}
 
 window.onload = main;
